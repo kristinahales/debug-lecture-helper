b5f45bfaaebcee29694ccf61ca31c1bfe7c0f94e
Krissy Hales <kristinakfriel@gmail.com>
2019-06-25 10:37:13 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     }
   }
 
-  componenetDidMount() {
+  componentDidMount() {
     console.log('componentDidMount method invoked...')
     const incrementTimer = () => this.setState({ timer: this.state.timer + 1 })
     setInterval(incrementTimer, 1000)
@@ -20,7 +20,7 @@ class App extends Component {
     return (
       <div className="App">
         <p>How many seconds have you been on my app?</p>
-        <p>{this.state.timer} seconds</p>
+        <p>{this.state.timer}seconds</p>
       </div>
     );
   }
